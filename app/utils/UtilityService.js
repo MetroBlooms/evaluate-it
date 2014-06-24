f28477dd73f0d619a5c0800f6578cd306b78f50b
gregsilverman <greg.marc.silverman@gmail.com>
2014-06-23 19:44:40 -0500

    geolocation rework

--- app/utils/UtilityService.js
+++ app/utils/UtilityService.js
@@ -143,7 +143,14 @@ Ext.define('EvaluateIt.utils.UtilityService', {
                 timeStamp = new Date(position.timestamp),
                 latitude = coordinates.latitude,
                 longitude =  coordinates.longitude,
-                accuracy = coordinates.accuracy;
+                accuracy = coordinates.accuracy,
+                panelOption;
+
+            // initialize sessionstorage items
+            sessionStorage.removeItem(latitude);
+            sessionStorage.removeItem(longitude);
+            sessionStorage.removeItem(timeStamp);
+            sessionStorage.removeItem(accuracy);
 
             alert('Position accuracy is ' + accuracy);
             console.log(' accuracy ' + accuracy);
@@ -164,7 +171,12 @@ Ext.define('EvaluateIt.utils.UtilityService', {
                 record.set('accuracy',accuracy);
                 record.set('timeStamp',timeStamp);
                 Ext.getStore('Geolocations').sync();
+
                 alert('Success! Location has been set with an accuracy of:' + accuracy);
+                // dispaly toolbar and google_map component
+                panelOption = 2;
+
+                EvaluateIt.utils.UtilityService.map_panel(record,panelOption);
             }
         }
 
@@ -188,7 +200,148 @@ Ext.define('EvaluateIt.utils.UtilityService', {
             watchID = null;
             alert('GPS stopped!');
         }
+    },
+
+    // Google maps API stuff here
+    map_panel: function(record,option){
+
+        var latitude = sessionStorage.latitude, //44.9616164;
+            longitude = sessionStorage.longitude; //-93.33539329999999,
+
+        console.log('latitude/longitude ...' + sessionStorage.latitude + ' ' + sessionStorage.longitude);
+
+        var	position = new google.maps.LatLng(latitude, longitude),
+
+            infowindow = new google.maps.InfoWindow({
+                content: 'EvaluateIt!'
+            }),
+
+        //Tracking Marker Image
+            image = new google.maps.MarkerImage(
+                'resources/images/point.png',
+                new google.maps.Size(32, 31),
+                new google.maps.Point(0, 0),
+                new google.maps.Point(16, 31)
+            ),
+
+            shadow = new google.maps.MarkerImage(
+                'resources/images/shadow.png',
+                new google.maps.Size(64, 52),
+                new google.maps.Point(0, 0),
+                new google.maps.Point(-5, 42)
+            ),
+
+            toolbar = Ext.create('Ext.Toolbar', {
+                docked: 'top',
+                alias : 'widget.geolocationToolbar',
+                ui: 'light',
+                defaults: {
+                    iconMask: true
+                },
+                items: [
+                    {
+                        xtype: 'button',
+                        ui: 'back',
+                        text: 'Back',
+                        // destroy form.Panel overlay and return to tree store view
+                        handler: function() {
+                            geo_panel.destroy();
+                        }
+                    },
+                    {
+                        xtype: 'button',
+                        itemId: 'stopWatch',
+                        text: 'StopWatch',
+                        iconCls: 'arrow_right',
+                        iconMask: true,
+                        handler: function() {
+                            EvaluateIt.utils.UtilityService.clear_watch();
+                        }
+                    },
+                    {
+                        xtype: 'selectfield',
+                        itemId: 'accuracy',
+                        autoSelect: false,
+                        placeHolder: 'accuracy',
+                        options: [
+                            {text: ''},
+                            {text: 'high',  value: 5},
+                            {text: 'med high',  value: 10},
+                            {text: 'medium',  value: 15},
+                            {text: 'med low',  value: 20},
+                            {text: 'low',  value: 60}
+                        ],
+                        listeners: {
+                            change: function(field, value) {
+                                if (value instanceof Ext.data.Model) {
+                                    value = value.get(field.getValueField());
+                                }
+                                console.log(value);
+                                // set accuracy as config variable
+                                EvaluateIt.config.accuracy = value;
+                            }
+                        }
+                    },
+                    {
+                        iconCls: 'home',
+                        handler: function() {
+                            google_map.getMap().panTo(position);
+                        }
+                    }
+                ]
+            });
+
+        var google_map = Ext.create('Ext.Map', {
+            alias : 'widget.whereAmI',
+
+            mapOptions : {
+                center : new google.maps.LatLng(latitude, longitude),
+                zoom : 12,
+                mapTypeId : google.maps.MapTypeId.ROADMAP,
+                navigationControl: true,
+                navigationControlOptions: {
+                    style: google.maps.NavigationControlStyle.DEFAULT
+                }
+            },
+
+            listeners: {
+                maprender: function(comp, map) {
+                    var marker = new google.maps.Marker({
+                        position: map.center,
+                        title : 'Weeeee!',
+                        map: map
+                    });
+
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infowindow.open(map, marker);
+                    });
+
+                    setTimeout(function() {
+                        map.panTo(position);
+                    }, 100);
+                }
+            }
+        });
+
+        // construct geo_panel components
+        if (option === 1) {
+            var obj = [toolbar];
+        }
+        if (option === 2) {
+            var obj = [toolbar, google_map];
+        }
+
+        var geo_panel = new Ext.form.Panel({
+            useCurrentLocation: true,
+            fullscreen: true,
+            layout: 'fit',
+            items: obj
+        });
+
+        geo_panel.setRecord(record);
+        geo_panel.show();
     }
+
 
 })
 
